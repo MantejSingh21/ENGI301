8a6b4a29e3518b163b60594d11509b589a42124b
Demo User <debian@beaglebone.localdomain>
2021-10-09 00:52:44 +0000

    Added code for movement

--- project_1/Bubba/motor_test.py
+++ project_1/Bubba/motor_test.py
@@ -11,101 +11,75 @@ servo3 = "P1_36"
 servo4 = "P1_33"
 
 
+def setup():
+    PWM.start(servo1, 5, 50, 0)
+    PWM.start(servo2, 5, 50, 0)
+    PWM.start(servo3, 5, 50, 0)
+    PWM.start(servo4, 5, 50, 0)
+
 def moveFrontRight():
-    
-    print("ID of process 1: {}".format(os.getpid()))
+
     print("Task 1 assigned to thread: {}".format(threading.current_thread().name))
 
- 
-        
     # Rotate the servo CCW 180 degrees
-    PWM.start(servo1,10,100)
+    PWM.set_duty_cycle(servo1, 10)
     time.sleep (0.1)
-    PWM.stop(servo1)
-    PWM.cleanup()
 
-    
-    PWM.start(servo1,30,100)
-    time.sleep (0.1)
-    PWM.stop(servo1)
-    PWM.cleanup() 
+    PWM.set_duty_cycle(servo1, 5)
 
     print("Front Right Motion cycle complete")
-    moveFrontLeft()
-        
+    
 def moveFrontLeft():
     
-    print("ID of process 2: {}".format(os.getpid()))
+
     print("Task 2 assigned to thread: {}".format(threading.current_thread().name))
     
-
-        
     # Rotate the servo CCW 180 degrees
-    PWM.start(servo2,10,100)
+    PWM.set_duty_cycle(servo2, 10)
     time.sleep (0.1)
-    PWM.stop(servo2)
-    PWM.cleanup()
 
-    
-    PWM.start(servo2,30,100)
-    time.sleep (0.1)
-    PWM.stop(servo2)
-    PWM.cleanup() 
+    PWM.set_duty_cycle(servo2, 5)
 
     print("Front Left Motion cycle complete")
-    moveBackRight()
 
 def moveBackRight():
     
-    print("ID of process 3: {}".format(os.getpid()))
     print("Task 3 assigned to thread: {}".format(threading.current_thread().name))
 
- 
-        
     # Rotate the servo CCW 180 degrees
-    PWM.start(servo3,10,100)
+    PWM.set_duty_cycle(servo3, 5)
     time.sleep (0.1)
-    PWM.stop(servo3)
-    PWM.cleanup()
 
-    
-    PWM.start(servo3,30,100)
-    time.sleep (0.1)
-    PWM.stop(servo3)
-    PWM.cleanup() 
+    PWM.set_duty_cycle(servo3, 20)
 
     print("Back Right Motion cycle complete")
-    moveBackLeft()
 
 def moveBackLeft():
     
-    print("ID of process 4: {}".format(os.getpid()))
     print("Task 4 assigned to thread: {}".format(threading.current_thread().name))
-
- 
-        
     # Rotate the servo CCW 180 degrees
-    PWM.start(servo4,10,100)
+    PWM.set_duty_cycle(servo4, 5)
     time.sleep (0.1)
-    PWM.stop(servo4)
-    PWM.cleanup()
 
-    
-    PWM.start(servo4,30,100)
-    time.sleep (0.1)
-    PWM.stop(servo4)
-    PWM.cleanup() 
+    PWM.set_duty_cycle(servo4, 15)
 
     print("Back Left Motion cycle complete")
-    moveFrontRight()
 
 if __name__ == "__main__":
     
-    moveFrontRight()
+    #moveFrontRight()
+    setup()
     
-    # t1 = threading.Thread(target=moveFrontRight, name="t1")
-    # t1.start()
-    # t2 = threading.Thread(target=moveFrontLeft, name="t2")
-    # t2.start()
-    # t1.join()
+    while(1):
+    
+        t1 = threading.Thread(target=moveFrontRight, name="t1")
+        t1.start()
+        t2 = threading.Thread(target=moveFrontLeft, name="t2")
+        t2.start()
+        t3 = threading.Thread(target=moveBackLeft, name="t3")
+        t3.start()
+        t4 = threading.Thread(target=moveBackRight, name="t4")
+        t4.start()
+        time.sleep(1)
+        # t1.join()
     
